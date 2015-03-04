f1ae63c47c6c4e185a1b71f93069377497296a5d
Héctor González Olmos <hector.gonzalez@enova.mx>
2015-03-04 09:21:21 -0600

    SiteMap

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -3,21 +3,23 @@ package bootstrap.liftweb
 import net.liftweb._
 import util._
 import Helpers._
-
+import code.model._
+import code.snippet._
 import common._
 import http._
 import sitemap._
 import Loc._
 import net.liftmodules.JQueryModule
 import net.liftweb.http.js.jquery._
-
 import code.config.MongoConfig
+import net.liftmodules.mongoauth._
+import net.liftmodules.mongoauth.MongoAuth
 
 /**
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
-class Boot {
+class Boot extends Loggable with Locs{
 
 
   def boot {
@@ -25,17 +27,54 @@ class Boot {
     // init mongodb
     MongoConfig.init()
 
+    MongoAuth.authUserMeta.default.set(User)
+
     // where to search snippet
     LiftRules.addToPackages("code")
 
     // Build SiteMap
-    
+    /*
     val entries = List(
       Menu.i("Home") / "index",
       Menu(Loc("Static", Link(List("static"), true, "/static/index"),
 	       "Static Content"))
       )
-    
+    */
+
+    val entries = List(
+      Menu.i("Home") /("index"), // the simple way to declare a menu
+      Menu.i("Signup") / "signup" >> RequireNotLoggedIn,
+      Menu.i("SignupComet") / "signup-comet",
+      Menu.i("Login") / "login" >> RequireNotLoggedIn,
+      Menu.i("Password Reset") / "password-reset",
+      Menu.i("Lost Password") / "lost-password",
+      Menu.i("Cart") / "cart",
+      Menu(Loc("logout", List("logout"), S.?("menu.logout"),
+        EarlyResponse(() => { User.logUserOut; S.redirectTo(S.referer openOr "/") }),
+        If(User.isLoggedIn _, S.?("logout.error"))
+      )),      
+      // more complex because this menu allows anything in the static path to be visible
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"), "Static Content")),
+      Menu.i("Admin Items") / "admin" / "item" / "index",
+      Menu.i("Admin Users") / "admin" / "users" / "index",   
+      Profile.profileParamMenu,
+      ViewItem.viewItemParam,
+      ViewItem.picsItemParam,    
+      Menu("Admin") / "admin" >> Hidden >> RequireLoggedIn,
+      Menu.i("Item List") / "item" / "index",
+      Menu.i("Item Create") / "item" / "create",
+      Menu.i("Item Edit") / "item" / "edit",
+      Menu.i("Item Lists") / "item" / "list",
+      Menu.i("Item View") / "item" / "view",
+      Menu.i("Item Admin") / "item" / "admin",
+      //Menu.i("List") / "comments" >> RequireLoggedIn,
+      Menu.i("Admin") / "admin" / * >> RequireLoggedIn,// >> loggedIn,
+      Menu.i("Error") / "error" >> Hidden,
+      Menu.i("404") / "404" >> Hidden,
+      Menu.i("403") / "403" >> Hidden,
+      Menu.i("Throw") / "throw" >> EarlyResponse(() => throw new Exception("This is only a test."))  
+    )
+
 
     // set the default htmlProperties
     LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))

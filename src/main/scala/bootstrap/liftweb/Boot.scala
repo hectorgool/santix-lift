2467edcd11c01760f0c62c4eac5496613bbf7723
Héctor González Olmos <hector.gonzalez@enova.mx>
2015-03-05 12:12:00 -0600

    add Admin Panel

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -3,17 +3,21 @@ package bootstrap.liftweb
 import net.liftweb._
 import util._
 import Helpers._
-import code.model._
-import code.snippet._
+
 import common._
 import http._
 import sitemap._
 import Loc._
 import net.liftmodules.JQueryModule
 import net.liftweb.http.js.jquery._
-import code.config.MongoConfig
-import net.liftmodules.mongoauth._
+
+import code.lib._
+import code.model._
+import code.snippet._
+//import code.snippet.products._
+
 import net.liftmodules.mongoauth.MongoAuth
+import net.liftmodules.mongoauth.Locs
 
 /**
  * A class that's instantiated early and run.  It allows the application
@@ -24,63 +28,67 @@ class Boot extends Loggable with Locs{
 
   def boot {
 
-    // init mongodb
-    MongoConfig.init()
-
+    //Could not initialize class net.liftmodules.mongoauth.model.ExtSession$
     MongoAuth.authUserMeta.default.set(User)
+
+    //by santo
+    MongoConfig.init
+
+    //by santo
+    SmtpMailer.init
+
+    //by santo
+    User.createSystemUser
+
+    //by santo
+    //CustomResourceId.init()
 
     // where to search snippet
     LiftRules.addToPackages("code")
 
-    // Build SiteMap
-    /*
-    val entries = List(
-      Menu.i("Home") / "index",
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"),
-	       "Static Content"))
-      )
-    */
+    //santo
+    // For S.loggedIn_? and TestCond.loggedIn/Out builtin snippet
+    LiftRules.loggedInTest = Full(() => User.isLoggedIn)
 
+    // checks for ExtSession cookie    
+    LiftRules.earlyInStateful.append(User.testForExtSession)
+
+    def ifIsLoggedIn = If(User.isLoggedIn _, strFuncToFailMsg(() => S.?("not.login")))
+
+    //>> If(() => S.loggedIn_?, () => RedirectResponse("/login"))
+
+    // Build SiteMap
     val entries = List(
       Menu.i("Home") /("index"), // the simple way to declare a menu
       Menu.i("Signup") / "signup" >> RequireNotLoggedIn,
-      Menu.i("SignupComet") / "signup-comet",
       Menu.i("Login") / "login" >> RequireNotLoggedIn,
-      Menu.i("Password Reset") / "password-reset",
-      Menu.i("Lost Password") / "lost-password",
+      Menu.i("Password Reset") / "password-reset" >> RequireNotLoggedIn,
+      Menu.i("Lost Password") / "lost-password" >> RequireNotLoggedIn,
       Menu.i("Cart") / "cart",
       Menu(Loc("logout", List("logout"), S.?("menu.logout"),
         EarlyResponse(() => { User.logUserOut; S.redirectTo(S.referer openOr "/") }),
         If(User.isLoggedIn _, S.?("logout.error"))
       )),      
-      // more complex because this menu allows anything in the static path to be visible
+      // more complex because this menu allows anything in the /static path to be visible
       Menu(Loc("Static", Link(List("static"), true, "/static/index"), "Static Content")),
-      Menu.i("Admin Items") / "admin" / "item" / "index",
-      Menu.i("Admin Users") / "admin" / "users" / "index",   
-      Profile.profileParamMenu,
-      ViewItem.viewItemParam,
-      ViewItem.picsItemParam,    
       Menu("Admin") / "admin" >> Hidden >> RequireLoggedIn,
+      Menu.i("Admin") / "admin" / * >> RequireLoggedIn,// >> loggedIn,
+      Menu.i("Admin Items") / "admin" / "item" / "index",
+      Menu.i("Admin Users") / "admin" / "users" / "index",
       Menu.i("Item List") / "item" / "index",
       Menu.i("Item Create") / "item" / "create",
       Menu.i("Item Edit") / "item" / "edit",
       Menu.i("Item Lists") / "item" / "list",
       Menu.i("Item View") / "item" / "view",
-      Menu.i("Item Admin") / "item" / "admin",
-      //Menu.i("List") / "comments" >> RequireLoggedIn,
-      Menu.i("Admin") / "admin" / * >> RequireLoggedIn,// >> loggedIn,
+      Menu.i("Item Admin") / "item" / "admin",      
       Menu.i("Error") / "error" >> Hidden,
       Menu.i("404") / "404" >> Hidden,
       Menu.i("403") / "403" >> Hidden,
-      Menu.i("Throw") / "throw" >> EarlyResponse(() => throw new Exception("This is only a test."))  
+      Menu.i("Throw") / "throw" >> Hidden >> EarlyResponse(() => throw new Exception("This is only a test.")),
+      Profile.profileParamMenu,
+      ViewItem.viewItemParam,
+      ViewItem.picsItemParam
     )
-
-
-    // set the default htmlProperties
-    LiftRules.htmlProperties.default.set((r: Req) => new Html5Properties(r.userAgent))
-
-    // Build SiteMap
-    //LiftRules.setSiteMap(Site.siteMap)
 
     // set the sitemap.  Note if you don't want access control for
     // each page, just comment this line out.
@@ -89,7 +97,7 @@ class Boot extends Loggable with Locs{
     //Show the spinny image when an Ajax call starts
     LiftRules.ajaxStart =
       Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-
+    
     // Make the spinny image go away when it ends
     LiftRules.ajaxEnd =
       Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
@@ -97,9 +105,13 @@ class Boot extends Loggable with Locs{
     // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
 
+    // Use HTML5 for rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))
+
     //Init the jQuery module, see http://liftweb.net/jquery for more information.
     LiftRules.jsArtifacts = JQueryArtifacts
-    JQueryModule.InitParam.JQuery=JQueryModule.JQuery191
+    JQueryModule.InitParam.JQuery=JQueryModule.JQuery172
     JQueryModule.init()
 
     //by santo
@@ -119,9 +131,32 @@ class Boot extends Loggable with Locs{
 
     //by santo
     LiftRules.responseTransformers.append {
-      case r if r.toResponse.code == 403 => RedirectResponse("403")
+      case r if r.toResponse.code == 403 => RedirectResponse("/403.html")
       case r => r
     }
+
+    //beta
+    //by santo
+    LiftRules.exceptionHandler.prepend {
+      case (runMode, request, exception) =>
+        logger.error("Boom! At "+request.uri)
+        InternalServerErrorResponse()
+    }
+
+    //You want to ensure clients are using HTTPS.
+    /*
+    LiftRules.earlyResponse.append { (req: Req) =>
+      //Amazon Load Balancer
+      //if (req.header("X-Forwarded-Proto") != Full("https") ) {
+      if (req.request.scheme != "https") {
+        val uriAndQuery = req.uri +
+        (req.request.queryString.map(s => "?"+s) openOr "")
+        val uri = "https://%s%s".format(req.request.serverName, uriAndQuery)
+        Full(PermRedirectResponse(uri, req, req.cookies: _*))
+      }
+      else Empty
+    } 
+    */   
 
   }
 

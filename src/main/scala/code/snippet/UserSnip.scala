7fb88f6148ae5952401d062f804e46f030de9e72
Héctor González Olmos <hector.gonzalez@enova.mx>
2015-03-18 16:12:12 -0600

    edit users table

--- src/main/scala/code/snippet/UserSnip.scala
+++ src/main/scala/code/snippet/UserSnip.scala
@@ -8,21 +8,26 @@ import util._
 import model.{User}
 import scala.xml._
 import util.Helpers._
+import org.joda.time.format.DateTimeFormat
+import com.foursquare.rogue.LiftRogue._
 
 
 class UserSnip extends Loggable {
 
 
+	private val fmt = DateTimeFormat.forPattern("dd/MM/yy")
+
 	def list = {
 
 	    val users = User.findAll
+	    //def users = User.where(_.verified eqs true).orderAsc(_.timecreated)
 
 	    "tbody tr * *" #> users.map { 
 	    	user =>
 	    		".id *"          #> Text( user.id.toString ) &
 	      		".username *"    #> Text( user.username.get ) &
 	      		".email *"       #> Text( user.email.toString ) &
-	      		".timecreated *" #> toInternetDate( user.timecreated.get ) &
+	      		".timecreated *" #> fmt.print( user.timecreated.get ) &
 	      		".verified *"    #> Text( user.verified.toString )
 
 	    }
